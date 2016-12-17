9a3127c6338d046428e94543c24bdc28f524afd5
duhaodong <du.haodong@xiaoyi.com>
2016-12-17 10:55:59 +0800

    avfilter 使用

---
+++ TestFFMpeg/ffmpeg/ffmpeg/TEST_Filter.h
@@ -0,0 +1,17 @@
+//
+//  TEST_Filter.h
+//  ffmpeg
+//
+//  Created by duhaodong on 2016/12/17.
+//  Copyright © 2016年 duhaodong. All rights reserved.
+//
+
+#ifndef TEST_Filter_h
+#define TEST_Filter_h
+
+#include <stdio.h>
+
+int filterFile(const char *inPutFile, const char*outPutFile);
+
+
+#endif /* TEST_Filter_h */
