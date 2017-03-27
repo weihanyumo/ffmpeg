82baf1aef3e36225c080120eaaf47386fe670db9
duhaodong <du.haodong@xiaoyi.com>
2017-03-27 14:19:00 +0800

    AVIOContext  读取网络流UDP

---
+++ TestFFMpeg/ffmpeg/ffmpeg/udpts/ffUDP.h
@@ -0,0 +1,13 @@
+//
+//  ffUDP.h
+//  ffmpeg
+//
+//  Created by duhaodong on 2017/3/27.
+//  Copyright © 2017年 duhaodong. All rights reserved.
+//
+
+#ifndef ffUDP_h
+#define ffUDP_h
+
+int ffUDP(char *url);
+#endif /* ffUDP_h */
